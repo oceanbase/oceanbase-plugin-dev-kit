4ccb2c0dcb664535c91d8ee962149289c9e23ff1
wangyunlai <wangyunlai.wyl@oceanbase.com>
2025-01-13 12:05:23 +0800

    fix fetch oceanbase source code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 [OceanBase](https://github.com/oceanbase/oceanbase) 插件开发包打包
+
+使用脚本打包
+```bash
+cd tools
+export OCEANBASE_GIT_REPO=git@gitlab.oceanbase-dev.com:oceanbase/oceanbase.git
+export OCEANBASE_GIT_TAG=fts4
+# build-rpm.sh <sourcepath> <package> <version> <release>
+bash build-rpm.sh $PWD/.. oceanbase-plugin-dev-kit 0.1.0 1
+```
+
+使用cmake打包
+```bash
+mkdir build
+cd build
+cmake .. -DOCEANBASE_GIT_REPO=git@gitlab.oceanbase-dev.com:oceanbase/oceanbase.git -DOCEANBASE_GIT_TAG=fts4
+cpack -G RPM
+```
--- cmake/FetchOceanBase.cmake
+++ cmake/FetchOceanBase.cmake
@@ -3,14 +3,9 @@ include(FetchContent)
 set(OCEANBASE_GIT_REPO "https://github.com/oceanbase/oceanbase.git" CACHE STRING "OceanBase git repository")
 set(OCEANBASE_GIT_TAG  "8d5a187da1dd96d1cac5b13c1af2616f2019250c"   CACHE STRING "OceanBase git tag or commit id to package")
 
-FetchContent_Declare(
-  oceanbase
-  GIT_REPOSITORY ${OCEANBASE_GIT_REPO}
-  GIT_TAG ${OCEANBASE_GIT_TAG})
-
-#FetchContent_MakeAvailable(oceanbase)
-
 FetchContent_Populate(
   oceanbase
-  SOURCE_DIR ${PROJECT_SOURCE_DIR}/deps/oceanbase
-  BINARY_DIR "")
+  GIT_REPOSITORY ${OCEANBASE_GIT_REPO}
+  GIT_TAG ${OCEANBASE_GIT_TAG}
+  GIT_PROGRESS TRUE
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/deps/oceanbase)
